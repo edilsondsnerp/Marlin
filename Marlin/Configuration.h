855e355077e5c38b915f61b130eb30cb9ef05409
edilsoncorrea <edilsoncorrea117@gmail.com>
2023-07-03 23:16:41 -0300

    Criado menu para ajuste de temperatura de PETG. Ajuste do pino do cooles da placa

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2236,11 +2236,11 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 230
-#define PREHEAT_2_TEMP_BED    70
-#define PREHEAT_2_TEMP_CHAMBER 35
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_3_LABEL       "ABS"
+#define PREHEAT_3_TEMP_HOTEND 230
+#define PREHEAT_3_TEMP_BED    70
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
 
 // @section motion
 
